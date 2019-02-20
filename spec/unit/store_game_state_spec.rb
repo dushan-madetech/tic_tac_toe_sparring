e90656ad91fba80f42b90e8047a81b00ad0257e3
MadeTech Dushan <dushan@madetech.com>
2019-02-20 16:14:27 +0000

    passing second and third unit tests in StoreGameState

--- spec/unit/store_game_state_spec.rb
+++ spec/unit/store_game_state_spec.rb
@@ -9,7 +9,7 @@ describe StoreGameState do
     end
   end
 
-  it 'can store a game state for an empty board' do 
+  it 'can store a game state for an empty board' do
     game_state_gateway = GameStateStorageGatewaySpy.new
     game_state_store = StoreGameState.new(game_state_gateway: game_state_gateway)
     game = [0,0,0,0,0,0,0,0,0]
@@ -18,4 +18,25 @@ describe StoreGameState do
     expect(last_game).to eq([0,0,0,0,0,0,0,0,0])
   end
 
+  it 'can store a game state for a board that has been modified by player moves' do
+    game_state_gateway = GameStateStorageGatewaySpy.new
+    game_state_store = StoreGameState.new(game_state_gateway: game_state_gateway)
+    game = [1,2,1,0,0,0,2,1,2]
+    game_state_store.execute(game)
+    last_game = game_state_gateway.last_game_saved
+    expect(last_game).to eq([1,2,1,0,0,0,2,1,2])
+  end
+
+  it 'can overwrrite an existing game state' do
+    game_state_gateway = GameStateStorageGatewaySpy.new
+    game_state_store = StoreGameState.new(game_state_gateway: game_state_gateway)
+    game = [0,0,0,0,0,0,0,0,0]
+    game_state_store.execute(game)
+    last_game = game_state_gateway.last_game_saved
+    expect(last_game).to eq([0,0,0,0,0,0,0,0,0])
+    game = [0,0,0,0,0,0,0,0,1]
+    game_state_store.execute(game)
+    last_game = game_state_gateway.last_game_saved
+    expect(last_game).to eq([0,0,0,0,0,0,0,0,1]) 
+  end
 end
