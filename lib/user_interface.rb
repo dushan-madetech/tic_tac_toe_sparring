f222a6fde4087a1a194a3a5310f7a36afe795daf
MadeTech Dushan <dushan@madetech.com>
2019-03-06 15:05:14 +0000

    rubocop changes

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -20,11 +20,13 @@ class UserInterface
   def initialize(board_gateway)
     @board_gateway = board_gateway
     @make_move = MakeMove.new(game_state_gateway: @board_gateway)
-    @determine_outcome = DetermineOutcome.new(game_state_gateway: @board_gateway)
+    @determine_outcome = DetermineOutcome.new(
+      game_state_gateway: @board_gateway
+    )
   end
 
   def execute
-    while(@determine_outcome.execute == 0) do
+    while @determine_outcome.execute.zero?
       board = @board_gateway.retrieve
       puts draw_grid(board.grid)
       @make_move.execute(request_input, board.current_player)
@@ -33,15 +35,13 @@ class UserInterface
     output_outcome(@determine_outcome.execute)
   end
 
-
-
   private
 
   def output_outcome(outcome_code)
     outcomes = {
-      1 => "Player 1 Wins!",
-      2 => "Player 2 Wins!",
-      3 => "Draw!"
+      1 => 'Player 1 Wins!',
+      2 => 'Player 2 Wins!',
+      3 => 'Draw!'
     }
     puts outcomes[outcome_code]
     outcomes[outcome_code]
@@ -49,11 +49,11 @@ class UserInterface
 
   def draw_grid(grid)
     output = "\n\n"
-    0.upto((grid.length)-1) do |position|
-      output << "#{grid[position] || position}"
+    0.upto(grid.length - 1) do |position|
+      output << (grid[position] || position).to_s
       case position % 3
-        when 0, 1 then output << " | "
-        when 2 then output << "\n-----------\n" unless position == (grid.length)-1
+      when 0, 1 then output << ' | '
+      when 2 then output << "\n-----------\n" unless position == grid.length - 1
       end
     end
     output << "\n"
