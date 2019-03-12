f33408e020942bebecd69c66362a03c67aa04796
Yusuf <yusuf@madetech.com>
2019-03-12 09:29:24 +0000

    Deleted duplicate files

--- lib/gateway/game_state_storage_gateway_spy.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class GameStateStorageGatewaySpy
-  attr_reader :last_game_saved
-
-  def save(game)
-    @last_game_saved = game
-  end
-end
--- lib/gateway/game_state_storage_gateway_stub.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class GameStateStorageGatewayStub
-  attr_writer :game_state
-
-  def retrieve
-    @game_state
-  end
-end
